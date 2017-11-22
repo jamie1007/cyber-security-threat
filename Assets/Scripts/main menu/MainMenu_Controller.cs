7c9cdc6398a99ff3d0278635152fcaaae0403a9e
jamie1007 <33902857+jamie1007@users.noreply.github.com>
2017-11-22 15:19:20 +0000

    Added project files

---
+++ Assets/Scripts/main menu/MainMenu_Controller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu_Controller : MonoBehaviour {
+    public Animator mainContentAnim;
+    public void onHideContent() {
+        mainContentAnim.SetBool("show",false);
+    }
+    public void onShowContent()
+    {
+        mainContentAnim.SetBool("show", true);
+    }
+}
