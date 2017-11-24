a0c182b6bd08bcae9790fa175cbc077c14eacd20
jamie1007 <33902857+jamie1007@users.noreply.github.com>
2017-11-24 12:40:15 +0000

    new annimation for start
    this has an annimation for the start button

--- Assets/Scripts/main menu/MainMenu_Controller.cs
+++ Assets/Scripts/main menu/MainMenu_Controller.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu_Controller : MonoBehaviour {
-    public Animator mainContentAnim;
-    public void onHideContent() {
-        mainContentAnim.SetBool("show",false);
+    public Animator mainContentAnim; //this is a refrance that
+    public void onHideContent() { //this is a function which is public so i can call it in the unity program
+        mainContentAnim.SetBool("show",false); //this will make the boolean value in the animator false making the program hide
     }
     public void onShowContent()
     {
