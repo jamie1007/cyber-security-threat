ece8b1f3f90e43de77496822902bb7649f0d4d95
jamie1007 <33902857+jamie1007@users.noreply.github.com>
2017-11-28 15:35:40 +0000

    code fully working (ish)
    both go at the same time

--- Assets/Scripts/main menu/MainMenu_Controller.cs
+++ Assets/Scripts/main menu/MainMenu_Controller.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class MainMenu_Controller : MonoBehaviour {
     public Animator mainContentAnim; //this is a refrance that
-    public void onHideContent() { //this is a function which is public so i can call it in the unity program
+    public void onHideContent()//this is a function which is public so i can call it in the unity program
+    { 
         mainContentAnim.SetBool("show",false); //this will make the boolean value in the animator false making the program hide
     }
     public void onShowContent()
---
+++ Assets/Scripts/main menu/Menu_controler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_controler : MonoBehaviour {
+    public Animator MenuAnim; //this is a refrance that
+    public void onHideContent()//this is a function which is public so i can call it in the unity program
+    {
+        MenuAnim.SetBool("show", false); //this will make the boolean value in the animator false making the program hide
+    }
+    public void onShowContent()
+    {
+        MenuAnim.SetBool("show", true);
+    }
+}
