bcebdead10d9c773fcddd0b5a124246d615e363a
jamie1007 <33902857+jamie1007@users.noreply.github.com>
2017-12-01 12:46:46 +0000

    made input page
    sim1 button links to sim1 input page

---
+++ Assets/Scripts/simulations/linkingSceneSim1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class linkingSceneSim1 : MonoBehaviour {
+    public void openPageSim() 
+    {
+        SceneManager.LoadScene("SimDataInput1", LoadSceneMode.Single); //this will close all pages and open the 'SimDataInput1'
+    }
+    public void openPageHome()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single); //this will load the main menu
+    }
+}
