cab3e301b06c5213e5b08b7db429ad0e7e393035
jamie1007 <33902857+jamie1007@users.noreply.github.com>
2017-12-05 15:36:09 +0000

    sim1input

---
+++ Assets/Scripts/simulations/dataInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dataInput : MonoBehaviour {
+    public Text spreadTxt;
+    public Text cyberSecurytTxt;
+    public Text populationTxt;
+    public void allInputsAccepted()
+    {
+        if (!string.IsNullOrEmpty(spreadTxt.text) && !string.IsNullOrEmpty(cyberSecurytTxt.text) && !string.IsNullOrEmpty(populationTxt.text))
+        {
+            
+        }
+        
+        else if()
+        {
+            
+        }
+         
+    }
+}
