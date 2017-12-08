50889ce29de4f2466b088e89dbd89cbafbc76911
jamie1007 <33902857+jamie1007@users.noreply.github.com>
2017-12-08 11:53:44 +0000

    default Poppulation

---
+++ Assets/Scripts/simulations/settingPopulation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class settingPopulation : MonoBehaviour {
+    public InputField populationTxt;
+
+    public void setValuePopulation() 
+    {
+        if (!string.IsNullOrEmpty(populationTxt.text))
+        {
+            populationTxt.text = "";
+        }
+        else
+        {
+            populationTxt.text = "65.64";
+        }
+    }
+}
