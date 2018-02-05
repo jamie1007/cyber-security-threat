49e90e8626e78f22553507e3d3e1ba5d1adbd3d8
jamie1007 <33902857+jamie1007@users.noreply.github.com>
2018-02-05 15:27:04 +0000

    finished table and spread calculations

--- Assets/Scripts/simulations/SpreadCalculator.cs
+++ Assets/Scripts/simulations/SpreadCalculator.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpreadCalculator : MonoBehaviour {
-
+    public Text InfectedP;
+    public Text InfectedNumber;
+    public Text NotInfectedP;
+    public Text NotInfectedNumber;
+    public Text CyberProtectedP;
+    public Text CyberProtectedNumber;
+    public static int Infected;
+    public static int NewInfected;
+    public static float cyberPrevent;
     public static int CardiffPopulation;
     public static int NorwichPopulation;
     public static int LondonPopulation;
@@ -13,10 +22,12 @@ public class SpreadCalculator : MonoBehaviour {
     // Use this for initialization
     void Start() {
         DefinePopulation();
+        Infected = 1;
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
     }
     public void DefinePopulation()
@@ -32,6 +43,13 @@ public class SpreadCalculator : MonoBehaviour {
     }
     public void Spreader()
     {
-
+        NewInfected = (int)Mathf.Pow(Infected, DataInput.spread); //this is calculating how many new computers are infected
+        Infected = Infected + NewInfected;
+    }
+    public void CyberRemove()
+    {
+        cyberPrevent = (int)(NewInfected * (float)DataInput.cyberSecurity / 10); //this takes a percentage of the cyber security level and states how many computers don't get infected
+        Mathf.Round(cyberPrevent);
+        Infected = Infected - (int)cyberPrevent;
     }
 }
